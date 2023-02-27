e73988f95d4abbe711eea7e50e65c7ab05bffa71
Mohamed Bendnaiba <mohamed.bendnaiba@vosker.com>
2023-02-27 10:55:03 +0100

    Update Storage_Tests.swift

--- Tests/Journify-Tests/Storage_Tests.swift
+++ Tests/Journify-Tests/Storage_Tests.swift
@@ -38,48 +38,47 @@ class StorageTests: XCTestCase {
     }
     
     //TODO: The test has failed. Please check this function
-//    func testEventWriting() throws {
-//        Journify.setup(with: Configuration(writeKey: "test"))
-//        Journify.shared().storage.hardReset(doYouKnowHowToUseThis: true)
-//
-//        var event = IdentifyEvent(userId: "benMed1", traits: try! JSON(with: MyTraits(email: "ben@med.com")))
-//        Journify.shared().storage.write(.events, value: event)
-//
-//        event = IdentifyEvent(userId: "benMed2", traits: try! JSON(with: MyTraits(email: "ben@med.com")))
-//        Journify.shared().storage.write(.events, value: event)
-//
-//        event = IdentifyEvent(userId: "benMed3", traits: try! JSON(with: MyTraits(email: "ben@med.com")))
-//        Journify.shared().storage.write(.events, value: event)
-//
-//        let results: [URL]? = Journify.shared().storage.read(.events)
-//
-//        XCTAssertNotNil(results)
-//
-//        let fileURL = results![0]
-//
-//        XCTAssertTrue(fileURL.isFileURL)
-//        XCTAssertTrue(fileURL.lastPathComponent == "0-journify-events.temp")
-//        XCTAssertTrue(FileManager.default.fileExists(atPath: fileURL.path))
-//
-//        let json = try! JSONSerialization.jsonObject(with: Data(contentsOf: fileURL), options: []) as! [String: Any]
-//
-//        let batch = json["batch"] as! [[String: Any]]
-//        let item1 = batch[0]["userId"] as! String
-//        let item2 = batch[1]["userId"] as! String
-//        let item3 = batch[2]["userId"] as! String
-//
-//        XCTAssertTrue(item1 == "benMed1")
-//        XCTAssertTrue(item2 == "benMed2")
-//        XCTAssertTrue(item3 == "benMed3")
-//
-//        Journify.shared().storage.remove(file: fileURL)
-//
-//        // make sure our original and temp files are named correctly, and gone.
-//        let originalFile = fileURL.deletingPathExtension()
-//        let tempFile = fileURL
-//        XCTAssertFalse(FileManager.default.fileExists(atPath: originalFile.path))
-//        XCTAssertFalse(FileManager.default.fileExists(atPath: tempFile.path))
-//    }
+    func testEventWriting() throws {
+        Journify.setup(with: Configuration(writeKey: "test"))
+        Journify.shared().storage.hardReset(doYouKnowHowToUseThis: true)
+
+        var event = IdentifyEvent(userId: "benMed1", traits: try! JSON(with: MyTraits(email: "ben@med.com")))
+        Journify.shared().storage.write(.events, value: event)
+
+        event = IdentifyEvent(userId: "benMed2", traits: try! JSON(with: MyTraits(email: "ben@med.com")))
+        Journify.shared().storage.write(.events, value: event)
+
+        event = IdentifyEvent(userId: "benMed3", traits: try! JSON(with: MyTraits(email: "ben@med.com")))
+        Journify.shared().storage.write(.events, value: event)
+
+        let results: [URL]? = Journify.shared().storage.read(.events)
+
+        XCTAssertNotNil(results)
+
+        let fileURL = results![0]
+
+        XCTAssertTrue(fileURL.isFileURL)
+        XCTAssertTrue(fileURL.lastPathComponent == "0-journify-events.temp")
+        XCTAssertTrue(FileManager.default.fileExists(atPath: fileURL.path))
+
+        let json = try! JSONSerialization.jsonObject(with: Data(contentsOf: fileURL), options: []) as! [[String: Any]]
+
+        let item1 = json[0]["userId"] as! String
+        let item2 = json[1]["userId"] as! String
+        let item3 = json[2]["userId"] as! String
+
+        XCTAssertTrue(item1 == "benMed1")
+        XCTAssertTrue(item2 == "benMed2")
+        XCTAssertTrue(item3 == "benMed3")
+
+        Journify.shared().storage.remove(file: fileURL)
+
+        // make sure our original and temp files are named correctly, and gone.
+        let originalFile = fileURL.deletingPathExtension()
+        let tempFile = fileURL
+        XCTAssertFalse(FileManager.default.fileExists(atPath: originalFile.path))
+        XCTAssertFalse(FileManager.default.fileExists(atPath: tempFile.path))
+    }
     
     func testFilePrepAndFinish() {
         Journify.setup(with: Configuration(writeKey: "test"))
