a5d709afc7a18efeaca13a957d60a9133d297f0f
yasser-chihab <yasser.chihab@journify.io>
2025-01-15 18:36:02 +0100

    add tag creation

--- release.sh
+++ release.sh
@@ -36,25 +36,8 @@ vercomp () {
 	return 0
 }
 
-# check if `gh` tool is installed.
-if ! command -v gh &> /dev/null
-then
-	echo "Github CLI tool is required, but could not be found."
-	echo "Install it via: $ brew install gh"
-	exit 1
-fi
 
-# check if `swift-create-xcframework` tool is installed.
-# command will return non-zero if not.
-if ! command -v swift-create-xcframework &> /dev/null
-then
-	echo "Swift's create-xcframework tool is required, but could not be found."
-	echo "Install it via:"
-    echo "    $ brew install mint"
-    echo "    $ mint install unsignedapps/swift-create-xcframework"
-    echo ""
-	exit 1
-fi
+
 
 # check if `gh` tool has auth access.
 # command will return non-zero if not auth'd.
